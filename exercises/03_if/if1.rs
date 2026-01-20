d3ba55583dd300da1a2c77406281bfae1f756a58
Christone007 <exellentemy@gmail.com>
2026-01-20 06:39:41 +0100

    Complete exercise 02

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,19 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a >= b {
+        a
+    } else {
+        b
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let a = 5;
+    let b = 9;
+
+    println!("The bigger number is {}", bigger(a, b));
 }
 
 // Don't mind this for now :)
